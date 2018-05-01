e65cdc2460934fa40e873e8722fe53ff64584c74
Lilun <iamlilun@taijun.net>
2018-05-01 20:10:23 +0800

    注入組件的store

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,23 @@
 </template>
 
 <script>
+    /**
+     * 可以在這import store..下面就不用this.$store..但不推薦..
+     * 因為變成每個component都要import..不如就在main引入..下層都能用this.$store
+     */
+//import store from './store';
+import {mapState, mapMutations} from 'vuex';
+
 export default {
-    computed:{
-        count(){
-            return this.$store.state.count; //取得vuex：state裡的count值
-        },
+    //用工且可以把所有state跟mutations塞進array,不然一個功能一個函式太累
+    computed: {
+        ...mapState(['count']), //es6語法..把這物件灑出來(@@啥東東?)
+        //這裡可以寫其它的computed
     },
-    methods:{
-        addCount(){
-            this.$store.commit('addCount'); //用commit來執行vuex：mutations下addCount()..
-        }
-    }
+    methods: {
+        ...mapMutations(['addCount']),
+        //這裡可以寫其它的methods
+    },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import store from './store';
 
 new Vue({
-  store, //直接引入vue..下面的component都能使用this.$store
+  store,
   el: '#app',
   render: h => h(App)
 })
