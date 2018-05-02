704a4ad08438d38edeb574b80769a41989c2b674
Lilun <iamlilun@taijun.net>
2018-05-02 22:06:26 +0800

    模組劃分

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div>
-    <h1>{{count}}</h1>
-    <!--<button @click="addCount">Add</button>--> <!--methods用1時,直接呼叫addCount-->
-    <!-- <button @click="add(2)">Add</button> --> <!--methods用2時,直接@click物件的key-->
-    <button @click="addCount(2)">Add</button> <!--methods用3時,可以帶參數設定Payload-->
-  </div>
 </template>
 
 <script>
-
-import {mapState, mapGetters} from 'vuex';
+import {mapState} from 'vuex';
 
 export default {
-    computed: {
-        ...mapState(['todos']),
+  mounted: {
+      //this.$store.dispatch('fetchList'); //功能同時存在兩個模組..所兩個都會執行
+      //this.$store.dispatch('todos/fetchList'); //module的namespaced設為ture時，就可以用/劃分只執行todos下的fetchList
 
-        //取得item還沒done的數量..如果其它組件都要用到..就把他提煉到store的getters
-        // itemsNotDone(){
-        //   return this.todos.filter(item => item.done).length;
-        // },
-        ...mapGetters(['itemsNotDone', 'itemsWithID']),
-    },
-    methods: { //直接呼叫
-        test(){
-           this.itemsWithID(2);
-        }
-    }
-
+      ...mapState('todos', [ //第一個參數為namespace
+          'list',
+          'text',
+          'input',
+          'current',
+          'loading',
+      ]),
+      ...mapState('member', [
+          'username',
+          'ranking',
+      ]),
+  }
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -3,31 +3,57 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
-    state: {
-        todos:[],
-    },
-    mutations : {
-        addCount(state, payload){
-            state.count += payload;
-        },
-    },
-    getters:{ //等於store的computed
-        itemsNotDone(state){ //吃state做為參數
-          return state.todos.filter(item => item.done).length;
-        },
-        itemsDone(state, getters){ //在第二個參數注入getters...也可以呼叫自己的function
-            return state.todos.length - getters.itemsNotDone;
-        },
-        // itemsWithID(state){ //也可以再包一層function..吃傳進來的參數
-        //     return (id) => {
-        //         return state.todos.filter(item => item.id === id);
-        //     }
-        // }
-        itemsWithID: state => //最終進化型
-                id => state.todos.filter(item => item.id === id),
+//可以劃分module..每個module有自己的store method
+const todos = {
+    namespaced: true, //不想跟其它模組actions method衝突時..可以加上namespaced的屬性
 
+    state: {
+        list: [],
     },
+    mutations:{
+        setList(state, payload){
+
+        }
+    },
+    getters:{
+        getLength(state, getters, rootState, rootGetters){
+            return rootState.member.username; //要取得別的module下的state要透過rootState參數
+        },
+    },
+    actions:{
+        fetchList(context){ //跟member的actions名字相同..所以app mounted時兩個都會執行
+
+            //沒rootCommit跟rootDispatch..類似功能要用第三個參數{root:true}
+            const {commit, dispatch, state, getters, rootState, rootGetter} = context;
+
+        }
+    }
+};
+
+const member = {
+    namespaced: true,
+    state:{
+        username: '',
+        rank: 0,
+    },
+    mutations: {
+        setList(){
+
+        }
+    },
+    actions: {
+        fetchList({commit}){
+            commit('todos/setList', [1,2,3,4], {root: true}); //commit別的module的mutations時..要加{root:true}
+            commit('setList', [1,2,3,4]); //commit自己的mutations
+        }
+    }
+};
+
+const store = new Vuex.Store({
+   modules:{ //注入module
+       todos: todos,
+       member: member,
+   }
 });
 
 export default store;
